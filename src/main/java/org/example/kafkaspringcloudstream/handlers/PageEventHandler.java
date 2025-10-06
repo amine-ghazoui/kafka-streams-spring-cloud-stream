2215907aa9cd9a8aa95eaceaf523bc19c2101a95
amine-ghazoui <ghazouimohamedamine@gmail.com>
2025-10-06 17:19:04 +0100

    Mise à jour des propriétés Spring Cloud Stream pour la configuration des topics T3 et T4

--- src/main/java/org/example/kafkaspringcloudstream/handlers/PageEventHandler.java
+++ src/main/java/org/example/kafkaspringcloudstream/handlers/PageEventHandler.java
@@ -1,5 +1,9 @@
 package org.example.kafkaspringcloudstream.handlers;
 
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
 import org.example.kafkaspringcloudstream.events.PageEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -7,6 +11,7 @@ import org.springframework.stereotype.Component;
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 @Component
@@ -22,6 +27,7 @@ public class PageEventHandler {
         };
     }
 
+    // ce supplier va fournir des PageEvent aleatoire
     @Bean
     public Supplier<PageEvent> pageEventSupplier() {
         return ()->{
@@ -32,5 +38,16 @@ public class PageEventHandler {
                     10+new Random().nextInt(10000)
             );
         };
+    }
+
+    // ce function va prendre un KStream<PageEvent> et le transformer en KStream<Long>
+    @Bean
+    public Function<KStream<String, PageEvent>,KStream<String, Long>> kStreamFunction(){
+        return (input)->
+            input.filter((k, v) -> v.duration() > 100)
+                    .map((k, v) -> new KeyValue<>(v.name(), v.duration()))
+                    .groupByKey(Grouped.with(Serdes.String(), Serdes.Long()))
+                    .count()
+                    .toStream();
     }
 }

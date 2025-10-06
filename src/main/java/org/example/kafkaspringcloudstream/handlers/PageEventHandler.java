c37da12cc32ecbc3ca17719caf63a7a8bd1ca548
amine-ghazoui <ghazouimohamedamine@gmail.com>
2025-10-06 17:48:40 +0100

    Ajout des méthodes publish et analytics dans PageEventController pour publier des événements et effectuer des analyses en temps réel

--- src/main/java/org/example/kafkaspringcloudstream/handlers/PageEventHandler.java
+++ src/main/java/org/example/kafkaspringcloudstream/handlers/PageEventHandler.java
@@ -4,10 +4,13 @@ import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.TimeWindows;
 import org.example.kafkaspringcloudstream.events.PageEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
@@ -47,7 +50,9 @@ public class PageEventHandler {
             input.filter((k, v) -> v.duration() > 100)
                     .map((k, v) -> new KeyValue<>(v.name(), v.duration()))
                     .groupByKey(Grouped.with(Serdes.String(), Serdes.Long()))
-                    .count()
-                    .toStream();
+                    .windowedBy(TimeWindows.of(Duration.ofSeconds(5000)))
+                    .count(Materialized.as("count-store"))
+                    .toStream()
+                    .map((k, v)-> new KeyValue<>(k.key(), v));
     }
 }
